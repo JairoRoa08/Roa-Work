efd0690552eb4c8aba18927f303d4f3de78973be
Jairo Roa <Jroa@usf.edu>
2025-02-02 21:49:36 -0500

    Task 2 - Modifying Customer Orders

--- cc_3.js
+++ cc_3.js
@@ -1,5 +1,15 @@
+// Task 1
 let prices = [0.99, 5.99, 10.99, 15.99, 20.99]
 prices.push (12.99);
 prices.shift();
 
 console.log("Updated Prices:", prices);
+
+// Task 2
+let orders = [1, 2, 3, 4, 5];
+orders [2] +=5;
+let totalOrders = orders.reduce((sum, quantity) => sum+ quantity, 0);
+
+console.log("Updated Orders:", orders);
+console.log("Total Order Count:", totalOrders);
+
